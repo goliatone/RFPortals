c7ff90b8f38a735311efc29e6bf570ffaa50b97b
goliatone <burgosemi@gmail.com>
2018-12-09 20:49:20 -0800

    update open portal request

--- RFPortals/Models/OpenPortalRequest.swift
+++ RFPortals/Models/OpenPortalRequest.swift
@@ -2,8 +2,20 @@
 //  OpenPortalRequest.swift
 //  RFPortals
 //
-//  Created by Emiliano Burgos on 12/9/18.
-//  Copyright © 2018 Emiliano Burgos. All rights reserved.
+//  Created by goliatone on 12/9/18.
+//  Copyright © 2018 goliatone. All rights reserved.
 //
 
 import Foundation
+
+struct OpenPortalRequest: Codable {
+    var userId: String
+    var portalAlias: String
+    var timestamp: Int
+    
+    private enum CodingKeys: String, CodingKey {
+        case userId = "user_id"
+        case portalAlias = "portal_alias"
+        case timestamp
+    }
+}
