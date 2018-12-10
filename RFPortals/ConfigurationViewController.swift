96ca19e3ae3394fdd24b2070ac5458ba25eb4705
goliatone <burgosemi@gmail.com>
2018-12-09 19:26:11 -0800

    update views

--- RFPortals/ConfigurationViewController.swift
+++ RFPortals/ConfigurationViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+class ConfigurationViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        print("jhere")
     }
 }
 
---
+++ RFPortals/Models/QRSearch.swift
@@ -0,0 +1,9 @@
+//
+//  QRSearch.swift
+//  RFPortals
+//
+//  Created by Emiliano Burgos on 12/9/18.
+//  Copyright © 2018 Emiliano Burgos. All rights reserved.
+//
+
+import Foundation
